4de152c256afd85d3cc7adf305de10815511a101
daniel.carrasco <daniel.carrasco@debian-8-amd64.fi.uncoma.edu.ar>
2023-09-06 14:51:18 -0300

    Modifico en Main, CuentaBanco y VerificarCuenta de tp03

--- src/tp03/CuentaBancaria.java
+++
@@ -1,9 +0,0 @@
-private int balance = 50;
-public CuentaBanco(){
-}
-public int getBalance(){
-return balance;
-}
-public void retiroBancario(int retiro){
-balance = balance - retiro;
-}
--- src/tp03/Main.java
+++ src/tp03/Main.java
@@ -1,4 +1,9 @@
-package tp03;
-
 public class Main {
+    public static void main(String[] args) {
+        VerificarCuenta vc = new VerificarCuenta();
+        Thread Luis = new Thread(vc, "Luis");
+        Thread Manuel = new Thread(vc, "Manuel");
+        Luis.start();
+        Manuel.start();
+        }
 }
