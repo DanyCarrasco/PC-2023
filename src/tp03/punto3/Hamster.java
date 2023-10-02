69a7b8cb1bfa39f18f27503bf0cbd1c536c90bad
daniel.carrasco <daniel.carrasco@debian-8-amd64.fi.uncoma.edu.ar>
2023-10-02 14:28:14 -0300

    Sincronizo nuevos metodos en Jaula y en el constructor de Hamster recibe como parametro la Jaula que comparten todos los hamsters creados en el Main3

--- src/tp03/punto3/Hamster.java
+++ src/tp03/punto3/Hamster.java
@@ -2,37 +2,32 @@ package tp03.punto3;
 
 public class Hamster implements Runnable {
     private Jaula jaula;
-    private boolean plato, rueda, hamaca;
+    // private boolean plato;// rueda, hamaca;
 
-    public Hamster() {
-        jaula = new Jaula();
-        this.plato = false;
-        this.rueda = false;
-        this.hamaca = false;
+    public Hamster(Jaula jaula) {
+        this.jaula = jaula;
+        // this.plato = false;
+        /*
+         * this.rueda = false;
+         * this.hamaca = false;
+         */
     }
 
     @Override
     public void run() {
-        while(!plato || !rueda || !hamaca){
-            if(jaula.usarPlato(Thread.currentThread().getName())){
-                plato = true;
-            } else {
-                System.out.println(Thread.currentThread().getName()+ " no puede usar el plato");
-            }
-
-            if(jaula.usarRueda(Thread.currentThread().getName())){
-                rueda = true;
-            } else {
-                System.out.println(Thread.currentThread().getName()+ " no puede usar la rueda");
-            }
-
-            if(jaula.usarHamaca(Thread.currentThread().getName())){
-                hamaca = true;
-            } else {
-                System.out.println(Thread.currentThread().getName()+ " no puede usar la hamaca");
-            }
+        System.out.println(Thread.currentThread().getName() + ". Listo.");
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        System.out.println(Thread.currentThread().getName() + " va por el plato");
+        jaula.usarPlato(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + " va por la rueda");
+        jaula.usarRueda(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + " va por la hamaca");
+        jaula.usarHamaca(Thread.currentThread().getName());
         System.out.println(Thread.currentThread().getName() + " termino");
     }
-} 
-
+}
--- src/tp03/punto3/Jaula.java
+++ src/tp03/punto3/Jaula.java
@@ -1,58 +1,74 @@
 package tp03.punto3;
 
 public class Jaula {
-    private Plato plato;
-    private Rueda rueda;
-    private Hamaca hamaca;
+    //private boolean plato; // rueda,hamaca;
+    /*
+     * private Plato plato;
+     * private Rueda rueda;
+     * private Hamaca hamaca;
+     */
 
     public Jaula() {
-        plato = new Plato();
-        rueda = new Rueda();
-        hamaca = new Hamaca();
+        //this.plato = true;
+        /*
+         * this.rueda = true;
+         * this.hamaca = true;
+         */
+        /*
+         * plato = new Plato();
+         * rueda = new Rueda();
+         * hamaca = new Hamaca();
+         */
     }
 
-    public synchronized boolean usarPlato(String nombre) {
-        boolean exito = false;
-        if (plato.estaDisponible()) {
-            plato.tomarPlato(nombre);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            plato.soltarPlato(nombre);
-            exito = true;
-        }
-        return exito;
+    public synchronized void usarPlato(String nombre) {
+        System.out.println(nombre + " toma plato");
+        tiempo();
+        System.out.println(nombre + " deja plato");
     }
 
-    public synchronized boolean usarRueda(String nombre) {
-        boolean exito = false;
-        if (rueda.estaDisponible()) {
-            rueda.tomarRueda(nombre);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            rueda.dejarRueda(nombre);
-            exito = true;
-        }
-        return exito;
+    public synchronized void usarRueda(String nombre) {
+        System.out.println(nombre + " toma rueda");
+        tiempo();
+        System.out.println(nombre + " deja rueda");
     }
 
-    public synchronized boolean usarHamaca(String nombre) {
-        boolean exito = false;
-        if (hamaca.estaDisponible()) {
-            hamaca.tomarHamaca(nombre);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            hamaca.dejarHamaca(nombre);
-            exito = true;
+    public synchronized void usarHamaca(String nombre) {
+        System.out.println(nombre + " toma hamaca");
+        tiempo();
+        System.out.println(nombre + " deja hamaca");
+    }
+
+    /*
+     * public synchronized boolean usarRueda(String nombre) {
+     * boolean exito = false;
+     * if (rueda.estaDisponible()) {
+     * rueda.tomarRueda(nombre);
+     * 
+     * rueda.dejarRueda(nombre);
+     * exito = true;
+     * }
+     * return exito;
+     * }
+     * 
+     * public synchronized boolean usarHamaca(String nombre) {
+     * boolean exito = false;
+     * if (hamaca.estaDisponible()) {
+     * hamaca.tomarHamaca(nombre);
+     * 
+     * hamaca.dejarHamaca(nombre);
+     * exito = true;
+     * }
+     * return exito;
+     * }
+     */
+
+    private void tiempo() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        return exito;
     }
 }
